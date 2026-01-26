61219de42b88a63af1fb642c8b91270780910c43
Marcio Moreira <magppmoreira@gmail.com>
2026-01-26 16:04:21 +0000

    Add files via upload

--- views/homeView.js
+++ views/homeView.js
@@ -1,10 +1,8 @@
 export function homeView() {
   return `
     <section class="text-center">
-      <h1 class="mb-3">Hi, I’m Márcio 👋</h1>
-      <p class="lead">
-        Full Stack Developer with Jedi discipline and clean code mojo.
-      </p>
+      <h1 class="mb-3"></h1>
+    
     </section>
   `;
 }
